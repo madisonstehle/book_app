321990f6c0cbfeb5d3a3dd8c17fc68986e7b1184
Tenaw_Derseh <mete439@gmail.com>
2020-02-08 15:16:02 -0800

    data base is created

---
+++ data/books.sql
@@ -0,0 +1,12 @@
+
+DROP TABLE IF EXISTS libraries;
+CREATE TABLE libraries (
+    id SERIAL PRIMARY KEY,
+    author VARCHAR(255),
+    title VARCHAR(255),
+    isbn VARCHAR(255),
+    image_url VARCHAR(255),
+    description TEXT,
+    bookshelf VARCHAR(255)
+);
+
